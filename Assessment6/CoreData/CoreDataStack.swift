3a172b550c448d381b4708c1805b5138f1c45ee8
Ethan Andersen <AndersenEthanG@gmail.com>
2021-05-21 10:17:37 -0600

    Storyboard modification

--- Assessment6/CoreData/CoreDataStack.swift
+++ Assessment6/CoreData/CoreDataStack.swift
@@ -5,4 +5,28 @@
 //  Created by Ethan Andersen on 5/21/21.
 //
 
-import Foundation
+import CoreData
+
+enum CoreDataStack {
+    static let container: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "<#CoreDataName#>")
+        container.loadPersistentStores { (storeDescription, error) in
+            if let error = error {
+                fatalError("Error loading persistent stores \(error.localizedDescription)")
+            }
+        }
+        return container
+    }()
+    
+    static var context: NSManagedObjectContext { container.viewContext }
+    
+    static func saveContext() {
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                print("Error saving context \(error)")
+            }
+        }
+    }
+} // End of Enum
